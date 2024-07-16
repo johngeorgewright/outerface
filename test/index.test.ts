637c66818b62528d11be136800a397d397f0e9ac
John Wright <jrite@protonmail.com>
2024-07-16 09:09:03 +0100

    test: makes tests simpler
    We only need to try and compile the files

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,11 +1,10 @@
-import { test } from 'vitest'
 import { outerface, Outerface } from '../src/index.js'
 
 interface ComparableStatic<T> extends Outerface<T> {
   compare(a: T, b: T): number
 }
 
-test('correct typing', () => {
+;() => {
   @outerface<ComparableStatic<TableCell>>()
   class TableCell {
     readonly value: number
@@ -18,10 +17,10 @@ test('correct typing', () => {
       return a.value - b.value
     }
   }
-  new TableCell(1)
-})
 
-test('typing error', () => {
+  new TableCell(1)
+}
+;() => {
   /*
   Argument of type 'typeof TableCell' is not assignable to parameter of type 'ComparableStatic<TableCell>'.
     Property 'compare' is missing in type 'TableCell' but required in type 'Comparable<TableCell>'.
@@ -36,4 +35,4 @@ test('typing error', () => {
     }
   }
   new TableCell(1)
-})
+}
